e80bc6b5177f6cc5661ebbc5c910299f0f27ae53
Patrick Fennema <patrick@bryce.demon.nl>
2019-10-11 16:38:52 +0200

    Added badge component

---
+++ frontend/src/components/VBadge.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="v-verified-badge">
+        <i
+            v-b-tooltip.hover title="This person has passed the background check and is verified" 
+            class="fa fa-check-circle-o fa-3x">
+        </i>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'v-verified-badge'
+    }
+</script>
+
+<style scoped lang="scss">
+    .v-verified-badge{
+        .fa{color:#238ae6;}
+    }
+</style>
