42d50baabf53f5b695bb286444ce89c1517a03fb
rick <rickw320@hotmail.com>
2019-10-11 16:41:33 +0200

    Evaluation form

---
+++ frontend/src/views/Evaluation.vue
@@ -0,0 +1,31 @@
+<template>
+    <b-container>
+       <iframe class="eval" src="https://docs.google.com/forms/d/e/1FAIpQLSfeT7bncpxv4Dn3rhB99Qak1gm_xOxvzmzsdV30XTQpzQ5W4w/viewform?embedded=true">
+            Loading…
+        </iframe>
+    </b-container>    
+</template>
+
+<script>
+    export default {
+        name: 'evaluation'
+    }
+</script>
+
+<style scoped lang="scss">
+// Occupy entire screen.
+.eval {
+    position: fixed; 
+    top: 0; 
+    left: 0; 
+    bottom: 0; 
+    right: 0; 
+    width: 100%; 
+    height: 100%; 
+    border: none; 
+    margin: 0; 
+    padding: 0; 
+    overflow: hidden; 
+    z-index:999999;
+}
+</style>
