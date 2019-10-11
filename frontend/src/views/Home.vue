df00c446766d8d24aaac98d51928658e0d76b984
Patrick Fennema <patrick@bryce.demon.nl>
2019-10-11 16:21:57 +0200

    update homepage

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,6 +1,13 @@
 <template>
-    <b-container>
-        home
+    <b-container fluid class="v-home">
+        <b-card>
+            <h3>Thank you for participating in the AirStorage prototype survey</h3>
+                On the next page, you will be presented with four possible listings.<br>
+                Out of these four options, please select the listing you <i>trust most</i>.<br>
+                After, you will be asked to fill in a short (~5 minutes) survey of your experience.<br>
+                Please press the button to start:<br><br>
+                <b-button variant="success" to="listings">Start</b-button>
+        </b-card>
     </b-container>
 </template>
 
@@ -11,4 +18,7 @@
 </script>
 
 <style scoped lang="scss">
+.v-home{
+    padding-top:80px;
+}
 </style>
